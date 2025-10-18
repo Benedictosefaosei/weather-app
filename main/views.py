43dc0965473d0dbcf078e2149fab1847eebb4cfa
benedictosefaosei <benedictosefaosei@gmail.com>
2025-10-18 13:15:51 -0400

    added the configurations

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .utils import get_weather_data, get_forecast_data
 
-# Create your views here.
+def index(request):
+    city = request.GET.get('city', 'Accra')
+    weather_data = get_weather_data(city)
+    forecast_data = get_forecast_data(city)
+    
+    context = {
+        'city': city,
+        'weather': weather_data,
+        'forecast': forecast_data['list'][:5],  # next 5 forecasts
+    }
+    return render(request, 'main/index.html', context)
